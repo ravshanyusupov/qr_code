5b9f932a963543f98a7671f731a865e28ab4f4e1
ravshan_yusupov <yusupovravshan001@gmail.com>
2022-11-21 10:09:09 +0500

    drgtyjyhtgrf

--- myapp/url.py
+++ myapp/url.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.main),
-    path('2864O8YN8KPUxl/', views.qr_code),
+    path('tom/srt=a2864O8YN8KPUxl', views.qr_code),
+    path('tom/srt=a2864O9YN8KPUxl', views.qr_code_1),
+    path('tom/srt=a286410YN8KPUxl', views.qr_code_2),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,11 @@ def main(request):
 
 def qr_code(request):
     return render(request, 'qr_code.html')
+
+
+def qr_code_1(request):
+    return render(request, 'qr_code_2.html')
+
+
+def qr_code_2(request):
+    return render(request, 'qr_code_3.html')
